d26211e0839480babfc1ecb2e27696311258e6b8
Keisuke Miyako <keisuke.miyako@4D.com>
2025-12-03 00:08:06 +0900

    msvc

--- build.rs
+++ build.rs
@@ -1,35 +1,29 @@
 fn main() {
-    // 1. Link C++ Standard Library
+
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-arg=-lc++");
-        // Link OpenMP on Mac (Homebrew path usually)
         println!("cargo:rustc-link-search=lib");
         println!("cargo:rustc-link-lib=static=omp");
-    } else if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-lib=stdc++");
-        println!("cargo:rustc-link-lib=gomp");
+        println!("cargo:rustc-link-lib=static=ctranslate2"); 
+        println!("cargo:rustc-link-lib=static=openblas");
+        println!("cargo:rustc-link-lib=static=spdlog");
+        println!("cargo:rustc-link-arg=-framework");
+        println!("cargo:rustc-link-arg=Accelerate");        
+    } 
+    
+    if cfg!(target_env = "msvc") {
+        println!("cargo:rustc-link-search=msvc");
+        println!("cargo:rustc-link-lib=static=cpu_features"); 
+        println!("cargo:rustc-link-lib=static=ctranslate2"); 
+        println!("cargo:rustc-link-lib=static=utils"); 
     }
-    // Windows connects MSVC OpenMP automatically usually.
 
-    // 2. Compile our C++ Bridge
     cxx_build::bridge("src/main.rs")
         .file("src/cpp/embedding_bridge.cc")
         .flag_if_supported("-std=c++17")
         .include("include") 
         .include("src/cpp") 
         .compile("ct2-bridge");
-
-    // 3. Link CTranslate2
-    // ideally, you should download/compile ctranslate2 separately and set CTRANSLATE2_ROOT.
-    // For this example, we assume libraries are in "lib" folder or installed globally.
-    println!("cargo:rustc-link-search=native=lib");
-    println!("cargo:rustc-link-lib=static=ctranslate2");
-    
-    // Dependencies of CTranslate2 (OpenBLAS/MKL/etc) need to be linked here.
-    // Example for OpenBLAS:
-    println!("cargo:rustc-link-lib=static=openblas");
-    println!("cargo:rustc-link-lib=static=spdlog");
-    // println!("cargo:rustc-link-lib=static=cpu_features");
 
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=src/cpp/embedding_bridge.cc");
